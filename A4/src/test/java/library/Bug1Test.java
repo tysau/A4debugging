afb49c18567ef39cc3a4238bf3ac75d8ff045b9a
genkidesu <tysaunders1@hotmail.com>
2022-10-16 23:08:47 +1100

    refactored junit tests

--- A4/src/test/java/library/Bug1Test.java
+++ A4/src/test/java/library/Bug1Test.java
@@ -35,9 +35,7 @@ public class Bug1Test {
     ReturnItemControl returnItem;
     PayFineControl payFine;
     
-    @Mock BorrowItemUI itemUI;
     @Mock ReturnItemUI returnItemUI;
-    @Mock PayFineUI payFineUI;
     
     public Bug1Test() {
     }
@@ -45,16 +43,18 @@ public class Bug1Test {
    
     @BeforeEach
     public void setUp() {
-    
+        // Create library, patron, item, calendar, loan, returnItem control
         library = Library.getInstance();
     
         patron = library.addPatron("John", "Smith", "dotcom", 1234);
         item = library.addItem("No", "Yes", "1", ItemType.BOOK);
         calendar = Calendar.getInstance();
         loan = library.issueLoan(item, patron);
+        
+        
         returnItem = new ReturnItemControl();
-        returnItemUI = spy(new ReturnItemUI(returnItem));
-
+        returnItemUI = new ReturnItemUI(returnItem);
+//        returnItem.setUi(returnItemUI);
     }
     
     @AfterEach
@@ -69,19 +69,18 @@ public class Bug1Test {
         // Setup
         Double quotedFee;
         Double actualFee;
-        
         calendar.incrementDate(3);
-        library.updateCurrentLoanStatus();        
-
-        Mockito.doNothing().when(returnItemUI).setReady();
-        
+        library.updateCurrentLoanStatus();    
+//        doNothing().when(returnItemUI).setReady();
+                
+      
         // Act
         returnItem.itemScanned(item.getId());
-        
+        // Get the quote displayed on screen
         quotedFee = library.calculateOverDueFine(loan);
-        
+        // End the loan
         returnItem.dischargeLoan(false);
-        
+        // Get the actual fines owed after return
         actualFee = patron.finesOwed();
         
         // Assert / results
--- A4/src/test/java/library/Bug2Test.java
+++ A4/src/test/java/library/Bug2Test.java
@@ -17,9 +17,11 @@ import library.entities.Item;
 import library.entities.ItemType;
 import library.entities.Library;
 import library.entities.Patron;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 public class Bug2Test {
 
     Library library;
@@ -28,10 +30,18 @@ public class Bug2Test {
     Item item1;
     Item item2;
     Item item3;
-    Item item4;
+    Item item4;    
+    int itemId1;
+    int itemId2;
+    int itemId3;
+    int itemId4;
+            
     
     @Mock BorrowItemUI itemUI;
     BorrowItemControl borrowItem;
+    
+    int expectedLoanTotal;
+    int actualLoanTotal;
     
     public Bug2Test() {
     }
@@ -39,17 +49,27 @@ public class Bug2Test {
     @BeforeEach
     public void setUp() {
     // Create a library, patron, items, and borrowItemUI/Control
-        library = Library.getInstance();
+    // And borrow 2 items.
     
+        library = Library.getInstance();
         patron = library.addPatron("John", "Smith", "dotcom", 1234);
         
         item1 = library.addItem("No", "Yes", "1", ItemType.BOOK);
         item2 = library.addItem("Because", "Why", "2", ItemType.BOOK);
         item3 = library.addItem("Below", "Above", "3", ItemType.BOOK);
         item4 = library.addItem("Inside", "Outside", "4", ItemType.BOOK);
-
+        itemId1 = item1.getId().intValue();
+        itemId2 = item2.getId().intValue();
+        itemId3 = item3.getId().intValue();
+        itemId4 = item4.getId().intValue();
+        
         borrowItem = new BorrowItemControl();
-        itemUI = spy(new BorrowItemUI(borrowItem));
+        borrowItem.setUI(itemUI);
+        
+        // Borrow 2 items to begin with
+        borrowItem.cardSwiped(patron.getId());
+        borrowItem.itemScanned(itemId1);
+        borrowItem.itemScanned(itemId2);
     }
     
 //    @AfterEach
@@ -58,30 +78,19 @@ public class Bug2Test {
     }
 
     /**
-     * Reproducing bug 2.1 - Patron can borrow more than 2 items.
-     */
+     * Reproducing bug 2.1 - Patron tries to borrow more than loan limit
+     * in a single borrowing session.
+     */    
     @Test
-    public void testBug2() {
+    public void testBug21() {
         // Setup
-        int expectedLoanTotal;
-        int actualLoanTotal;
-
-        Mockito.doNothing().when(itemUI).setReady();
-        Mockito.doNothing().when(itemUI).setScanning();
-        Mockito.doNothing().when(itemUI).setFinalising();
-        Mockito.doNothing().when(itemUI).setCompleted();
-        Mockito.doNothing().when(itemUI).display(any(Object.class));
         
-        // Act
-        // Attempt to borrow 3 items
-        borrowItem.cardSwiped(patron.getId());
-        borrowItem.itemScanned(5);
-        borrowItem.itemScanned(6);
-        
+        // Act - Try to borrow a third item
         try {
-            borrowItem.itemScanned(7);
+            borrowItem.itemScanned(itemId3);
         }
         catch (Exception e) {
+            System.out.println("Script 2.1 - Third item refused - Borrow limit");
         }
         
         borrowItem.commitLoans();
@@ -89,57 +98,47 @@ public class Bug2Test {
         // Assert / Results
         expectedLoanTotal = 2;
         actualLoanTotal = patron.getNumberOfCurrentLoans();
-        System.out.println("Script 2.1 - Loans, expected: " + expectedLoanTotal + 
+        
+        System.out.println("Script 2.1 - Loans expected: " + expectedLoanTotal + 
                 " Loans actual: " + actualLoanTotal); 
         
-        assertEquals(expectedLoanTotal, actualLoanTotal);
-    } 
-    
+        assertEquals(expectedLoanTotal, actualLoanTotal);        
+    }    
+
     /**
-     * Reproducing bug 2A - Patron can borrow additional item in later visit.
+     * Reproducing bug 2.2 - Patron can borrow additional item in later visit.
      */
     @Test
-    public void testBug2a() {
+    public void testBug22() {
         // Setup
-        int expectedLoanTotal;
-        int actualLoanTotal;
-
-        Mockito.doNothing().when(itemUI).setReady();
-        Mockito.doNothing().when(itemUI).setScanning();
-        Mockito.doNothing().when(itemUI).setFinalising();
-        Mockito.doNothing().when(itemUI).setCompleted();
-        Mockito.doNothing().when(itemUI).display(any(Object.class));
         
-        // Act:
-        // Borrowing session 1
-        borrowItem.cardSwiped(patron.getId());   
-        borrowItem.itemScanned(1);
-        borrowItem.itemScanned(2);
-        
+        // Act: Try to borrow a third item       
         try {
-        borrowItem.itemScanned(3);
+            borrowItem.itemScanned(itemId3);
         }
         catch (Exception e) {
+            System.out.println("Script 2.2 - Third item refused - Borrow limit");        
         }
         borrowItem.commitLoans();
-        
-        // Borriwing session 2
-        borrowItem = new BorrowItemControl();
-        itemUI = spy(new BorrowItemUI(borrowItem));
 
+        // Act: Return again and swipe card
+        borrowItem = new BorrowItemControl();
+        borrowItem.setUI(itemUI);
         borrowItem.cardSwiped(patron.getId());
         
+        // Try to borrow an extra item
         try {
-        borrowItem.itemScanned(4);
-        borrowItem.commitLoans();
+            borrowItem.itemScanned(itemId4);
+            borrowItem.commitLoans();
         }
         catch (Exception e) {
+            System.out.println("Script 2.2 - Borrow extra item rejected");
         }
   
         // Assert / Results
         expectedLoanTotal = 2;        
         actualLoanTotal = patron.getNumberOfCurrentLoans();
-        System.out.println("Script 2.2 - Loans, expected: " + expectedLoanTotal + 
+        System.out.println("Script 2.2 - Loans expected: " + expectedLoanTotal + 
                 " Loans actual: " + actualLoanTotal);  
         
         assertEquals(expectedLoanTotal, actualLoanTotal);
